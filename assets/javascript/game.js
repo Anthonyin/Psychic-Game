b4ca629d50f2c195fc4aeae7dbf9f05f520c724b
Anthony Yin <anthony923x@gmail.com>
2018-06-27 00:36:03 -0400

    one more thing to code-reset when match the letter picked

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,50 +1,78 @@
-var computerChoose = [
-  'a',
-  'b',
-  'c',
-  'd',
-  'e',
-  'f',
-  'g',
-  'h',
-  'i',
-  'j',
-  'k',
-  'l',
-  'm',
-  'n',
-  'o',
-  'p',
-  'q',
-  'r',
-  's',
-  't',
-  'u',
-  'v',
-  'w',
-  'x',
-  'y',
-  'z'
-];
-var win = 0;
-var lose = 0;
-var guestLeft = 9;
-var guestPick = [];
-var computerGuess =
-  computerChoose[Math.floor(Math.random() * computerChoose.length)];
-console.log(computerGuess);
-document.onkeyup = function(event) {
-  var userGuess = event.key;
-  console.log(userGuess);
-  if (computerGuess === userGuess) {
-    win++;
-    guestLeft = 9;
-    guestPick = [];
-  } else {
-    guestLeft--;
-    guestPick.push[userGuess];
+$(document).ready(function() {
+  // var letters = 'asdasdasd'.split('')
+  var computerChoose = [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z'
+  ];
+  var win = 0;
+  var lose = 0;
+  var guessLeft = 9;
+  var guessPick = [];
+
+  var userGuess = '';
+
+  function reset() {
+    guessLeft = 9;
+    guessPick = [];
   }
-  if (guestLeft === 0) {
-    lose++;
+
+  reset();
+
+  var computerGuess = (computerGuess =
+    computerChoose[Math.floor(Math.random() * computerChoose.length)]);
+  console.log(computerGuess);
+
+  document.onkeyup = function(event) {
+    userGuess = event.key;
+    console.log(userGuess);
+    checkGuess();
+    appendCounter();
+  };
+
+  function checkGuess() {
+    if (computerGuess === userGuess) {
+      win++;
+      reset();
+    }
+
+    if (computerGuess !== userGuess) {
+      guessLeft--;
+      guessPick.push(userGuess);
+    }
+    if (guessLeft === 0) {
+      lose++;
+      reset();
+    }
   }
-};
+
+  function appendCounter() {
+    $('#wins').text('Your wins are:' + win);
+    $('#losses').text('Your lose are:' + lose);
+    $('#guessesLeft').text('You are left:' + guessLeft);
+    $('#YouguessSofar').text('You alreadyt picked:' + guessPick);
+  }
+});
